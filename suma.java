8a01372e04ed38782ff3890362ff52d466343203
Ajay <Ajay@something.ing>
2018-09-19 18:52:36 +0200

    Canviat a catala.

--- suma.java
+++ suma.java
@@ -1,8 +1,8 @@
 int num1 =0;
 int num2 =0;
 
-System.out.println("Intoduce el primer numero:");
+System.out.println("Introdueix el primer numero:");
 num1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
+System.out.println("Introdueix el segon numero:");
 num2 = reader.nextInt();
