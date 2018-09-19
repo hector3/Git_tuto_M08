a04d3ff2e1d10c88da33e936115ef32a314c0950
Ajay <Ajay@something.ing>
2018-09-19 18:46:40 +0200

    modified text to be in english

--- suma.java
+++ suma.java
@@ -1,8 +1,8 @@
 int num1 =0;
 int num2 =0;
 
-System.out.println("Intoduce el primer numero:");
+System.out.println("First number:");
 num1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
+System.out.println("Second number:");
 num2 = reader.nextInt();
