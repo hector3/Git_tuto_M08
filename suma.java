987e5b6604d3cd76ebffd4ab21c77af96ded3bda
Ajay <Ajay@something.ing>
2018-09-19 18:42:27 +0200

    Added a new java file.
    Sume.java gets 2 numbers form the user and dose nothing.

---
+++ suma.java
@@ -0,0 +1,8 @@
+int num1 =0;
+int num2 =0;
+
+System.out.println("Intoduce el primer numero:");
+num1 = reader.nextInt();
+
+System.out.println("Introduce el segundo numero:");
+num2 = reader.nextInt();
